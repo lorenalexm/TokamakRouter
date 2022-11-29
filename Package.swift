56c38324dae50d1c138ff3372f7cea3083b76239
Alex Loren <lorenalexm@gmail.com>
2022-11-29 05:37:39 -0800

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.6
+import PackageDescription
+let package = Package(
+    name: "TokamakRouter",
+	platforms: [.macOS(.v11)],
+    products: [
+        .library(name: "TokamakRouter", targets: ["TokamakRouter"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/TokamakUI/Tokamak", from: "0.11.0")
+    ],
+    targets: [
+        .target(
+            name: "TokamakRouter",
+            dependencies: [
+                .product(name: "TokamakShim", package: "Tokamak")
+            ]),
+        .testTarget(
+            name: "TokamakRouterTests",
+            dependencies: ["TokamakRouter"]),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+# TokamakRouter
+
+> Simple location hash-based navigation for Tokamak web apps.
+
+TokamakRouter is based on the amazing work of [frzi/SwiftUIRouter](https://github.com/frzi/SwiftUIRouter/), which brings path-based routing to SwiftUI. This repository hopes to bring a similar solution to [Tokamak](https://tokamak.dev).
